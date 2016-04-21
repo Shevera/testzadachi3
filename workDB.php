48d98e5be88caef51b70d4961f8387c68c781fc4
Andre <screppi@gmail.com>
2016-04-21 19:34:57 +0300

    test commit

--- workDB.php
+++ workDB.php
@@ -8,58 +8,80 @@ function checkPostParams(){
     $email          = isset($_POST['email'])    ? $_POST['email']    : null;
     $message        = isset($_POST['message'])  ? $_POST['message']  : null;
     $myFile         = isset($_FILES['myFile'])  ? $_FILES['myFile']  : null;
+    $user_ip        = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+    $user_browser   = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+    $created_data   = date('Y:m:d H:m:s');
 
     //Защита от плохишей
     //обрезаем пробелы,перекодируем в html, экранируем спец символы
-    $name= trim(htmlspecialchars(mysql_real_escape_string($name)));
-    $email= trim(htmlspecialchars(mysql_real_escape_string($email)));
-    $message= trim(htmlspecialchars(mysql_real_escape_string($message)));
+    $name= trim(htmlspecialchars($name));
+    $email= trim(htmlspecialchars($email));
+    $message= trim(htmlspecialchars($message));
 
     //Проверка файла и запись его в переменную
-    if(checkFile($myFile)){
-        $myFile = checkFile($myFile);
-    }else{
-        $myFile = null;
-    }
+    //возвращаем урл к файлу
+     $myFile = checkFile($myFile);
+
 
     //Создаем массив с значениями переменных
-    $result[] = [
+    $result = [
                 'name'      => $name,
                 'email'     => $email,
                 'message'   => $message,
-                'myFile'    => $myFile
+                'myFile'    => $myFile,
+                'user_ip'   => $user_ip,
+                'user_browser' => $user_browser,
+                'created_data' => $created_data
               ];
 
     return $result;
 }
 
 function checkFile($myFile){
-    $CorrectType = ['image/gif','image/jpeg', 'image/pjpeg', 'text/html'];
+
+    // Каталог, в который мы будем принимать файл:
+    $uploaddir = './files/';
+    $uploadfile = $uploaddir.basename($_FILES['myFile']['name']);
+
+    //правильные типы
+    $CorrectType = ['image/gif','image/jpeg', 'text/html'];
     $flag = false;
 
-    //проверка типа файл
+    echo "<hr>";
+    var_dump($myFile) ;
+    echo "<hr>";
+
+   // проверка типа файл
     foreach ($CorrectType as $type){
-        if($type === $myFile[type]){
+        if($type === $myFile['type']){
             $flag = true;
-            break;
+
         }
     }
 
     //проверка размера файла не больше 0,5 Mb и корректный тип данных
-    if($myFile[size] > 500000 && $flag !== true){
-        return $myFile=null;
+    if($myFile['myFile']['size'] > 500000 && $flag !== true){
+        return $myFile = null;
     }
-    //бинарное копирование файла в переменную
-    $temp=fopen($_POST['tmp_name'],'rb'); //  открыли файл на чтение
-    $myFile = fread($temp,filesize($_POST['name']));//считали файл в переменную
-    fclose($temp);//закрыли файл
-    //защита переменной от опасных символов
-    $myFile=addslashes($myFile);
 
-    return $myFile;
+    if($flag==true){
+        echo "Flag is true";
+    }else{
+        echo "Flag is false";
+    }
+
+    // Копируем файл из каталога для временного хранения файлов:
+    if ($flag && (copy($_FILES['myFile']['tmp_name'], $uploadfile)))
+    {
+        echo "<h3>Файл успешно загружен на сервер</h3>";
+        return $myFile = $_FILES['myFile']['name'];
+    }else{
+        echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>";
+        return $myFile = null;
+    }
+
 }
 
-// Array ( [name] => testimage.jpg [type] => image/jpeg [tmp_name] => C:\OpenServer\userdata\temp\php696A.tmp [error] => 0 [size] => 41467 )
 
 //проверяем или капча введена правильно
 function checkCaptcha($captcha){
@@ -68,71 +90,8 @@ function checkCaptcha($captcha){
 
     if($captcha === $captchaServer)
         return true;
-    return false;
-}
-/*
-function checkFile(){
-//    name=uploadfile для файл
-// http://php.spb.ru/php/image.html - запись картинок и текста в базу почитать
-
-// Каталог, в который мы будем принимать файл:
-    $uploaddir = './files/';
-    $uploadfile = $uploaddir.basename($_FILES['uploadfile']['name']);
-// Массив с правильными форматами
-    $mimeFormat = array('image/gif','image/jpeg', 'image/pjpeg', 'text/html');
-    $flag = true;
-
-//путь к временному файлу
-    $uploadfileTemp =  $_FILES['uploadfile']['tmp_name'];
-
-//Размер загруженого файла не больше 500 кб (0,5 мб)
-    if($flag){
-        $fileSize = filesize($uploadfileTemp);
-        if($fileSize > 500000){
-            echo 'Размер не правильный';
-            $flag = false;
-        }
-    }
-
-
-//Проверяем или формат принятого файла соответствует заданным форматам
-    if($flag){
-
-        //создаем мимо переменные
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        $formatFile = finfo_file($finfo, $uploadfileTemp);
-        finfo_close($finfo);
-
-        foreach ($mimeFormat as $mime){
-            if($mime === $formatFile){
-                $flag = true;
-                break;
-            }else{
-                $flag = false ;
-            }
-        }
-    }
-
-
-    // Копируем файл из каталога для временного хранения файлов:
-    if ($flag && (copy($_FILES['uploadfile']['tmp_name'], $uploadfile)))
-    {
-        echo "<h3>Файл успешно загружен на сервер</h3>";
-        return true;
-    }else{
-        echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>";
         return false;
-    }
-
-
-// Выводим информацию о загруженном файле:
-    echo "<h3>Информация о загруженном на сервер файле: </h3>";
-    echo "<p><b>Оригинальное имя загруженного файла: ".$_FILES['uploadfile']['name']."</b></p>";
-    echo "<p><b>Mime-тип загруженного файла: ".$_FILES['uploadfile']['type']."</b></p>";
-    echo "<p><b>Размер загруженного файла в байтах: ".$_FILES['uploadfile']['size']."</b></p>";
-    echo "<p><b>Временное имя файла: ".$_FILES['uploadfile']['tmp_name']."</b></p>";
 }
-*/
 
 function connect_db(){
     $mysqli = new mysqli('localhost', 'root', '' , 'testzad3');
@@ -155,25 +114,30 @@ function select_post(){
     return $row;
 }
 
-function insert_post(array $result){
+function insert_post($result){
+    print_r($result);
+     $name           = $result['name'];
+     $email          = $result['email'];
+     $message        = $result['message'];
+     $myFile         = $result['myFile'];
+     $user_ip        = $result['user_ip'];
+     $user_browser   = $result['user_browser'];
+     $created_data   = $result['created_data'];
 
-    if($result[error]){
-        $userName = $result[userName];
-        $email =    $result[email];
-        $text =     $result[text];
-        $homepage = $result[homepage];
-        $user_ip =  $result[user_ip];
-        $user_browser = $result[user_browser];
-        $created_data = $result[created_data];
-        $data = $result[data];
-
-    }else{
-        return false;
-    }
 
     $mysqli= connect_db();
-    $sql = "INSERT INTO post( 'id', 'userName',     'email',    'text',   'homepage',     'user_ip',  'user_browser',     'created_data', 'data')
-                      VALUES (null, '{$userName}', '{$email}', '{$text}', '{$homepage}', '{$user_ip}', '{$user_browser}', '{$created_data}' , '{$data}')";
+    $sql = "INSERT INTO `post` ( `userName`, `email`,      `text`,       `user_ip`,    `user_browser`,    `created_data`,   `url`)
+                        VALUES ('{$name}',  '{$email}', '{$message}', '{$user_ip}', '{$user_browser}', '{$created_data}' ,'". $result['myFile']."')";
+//print_r($sql);
+    /*Debug
+    $sql1=" INSERT INTO `post`(`id`, `userName`, `email`, `text`, `user_ip`, `user_browser`, `created_data`, `data`)
+    VALUES (NULL,'asdasd', 'screppi@gmail.com', 'ddfhfh', '127.0.0.1', NULL, '2016:04:21 13:04:53', NULL)";
+
+    echo $sql;
+    echo "<hr>";
+    echo $sql1;
+
+*/
     if ($mysqli->query($sql)) {
         echo "New record created successfully";
     } else {
@@ -203,7 +167,9 @@ function get_content($tableColumn, $sort){
     $start=abs($page*$per_page);
 
     $mysqli= connect_db();
-    $sql = "SELECT * FROM post ORDER BY {$tableColumn} {$sort} LIMIT {$start},{$per_page}";
+    $sql = "SELECT * FROM post
+            ORDER BY {$tableColumn} {$sort}
+            LIMIT {$start},{$per_page}";
     $res = $mysqli->query($sql);
 
         //определяем количество записей в таблице всего
